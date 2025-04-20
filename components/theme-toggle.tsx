3d77e1e4852c5e667c5829a329df5d50ea366570
pranithaiwiz <pranithkumar.8802@gmail.com>
2025-04-20 09:49:18 +0530

    .

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -9,18 +9,14 @@ export function ThemeToggle() {
   return (
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="p-2 rounded-md transition-all duration-300
-      bg-gray-200 text-blue-500 
-      hover:text-blue-700 
-      border border-blue-400 
-      ring-1 ring-inset ring-blue-300 
-      hover:shadow-lg 
-      shadow-[0_0_10px_2px_rgba(59,130,246,0.2)] 
-      dark:bg-black dark:text-green-600 
-      dark:hover:text-white 
-      dark:border dark:border-green-600 
+      className="p-2 rounded-md transition-all duration-300 
+      bg-gray-200 text-gray-500 
+      hover:text-gray-700 shadow-md 
+      dark:bg-black dark:border dark:border-green-600 dark:text-green-400 
+      ring-1 ring-inset ring-gray-300 
       dark:ring-green-500 
-      dark:shadow-[0_0_15px_2px_rgba(34,197,94,0.4)]"
+      hover:shadow-lg 
+      dark:shadow-[0_0_15px_2px_rgba(34,197,94,0.5)]"
     >
       {theme === "dark" ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
     </button>
